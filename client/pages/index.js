5232fb500786962786bc013dc18a2f555a62b5e1
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-04-21 21:17:33 -0400

    got a little something going here

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,19 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { io } from "socket.io-client";
+import { useState } from "react";
+const socket = io("http://localhost:8000", { transports: ["websocket"] });
 
 export default function Home() {
+  const chatInput = document.getElementById("chatInput");
+  const [message, setMessage] = useState(chatInput);
+
+  const handlePost = (ev) => {
+    console.log(ev);
+    socket.emit("postMessage", { post: message });
+    chatInput.value = "";
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +25,27 @@ export default function Home() {
         <header className={styles.header}>
           <h1 className={styles.title}>socket.io chat</h1>
         </header>
-        <div className={styles.subcontainer}>stuff</div>
+        <div className={styles.subcontainer}>
+          <ul className={styles.messages}></ul>
+          <form
+            onSubmit={(ev) => {
+              ev.preventDefault();
+              handlePost(ev);
+            }}
+            className={styles.input}
+          >
+            <input
+              key="chatInput"
+              id="chatInput"
+              type="text"
+              onChange={(ev) => {
+                console.log(ev);
+                setMessage(ev.target.value);
+              }}
+            />
+            <input type="submit" />
+          </form>
+        </div>
       </main>
     </div>
   );
